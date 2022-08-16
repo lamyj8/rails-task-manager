17983182707abf170884cc28acf1146f2b89884f
Julien Lamy <lamy.j8@gmail.com>
2022-08-16 17:00:43 +0200

    exo7OK

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,4 +18,36 @@ class TasksController < ApplicationController
 
     redirect_to task_path(@task)
   end
+
+  def edit # page: form pré-repli
+    @task = Task.find(params[:id])
+  end
+
+  def update # réception des data du form
+    # params:
+    # {
+    #   "task"=>{"title"=>"Laundry", "detail"=>"lorem ipsum", "completed"=>"false"},
+    #   "id"=>"3"
+    # }
+
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :detail, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,13 @@ Rails.application.routes.draw do
  get "tasks", to: "tasks#index"
 
  # READ ONE
-
  get "tasks/:id", to: "tasks#show", as: "task"
+
+  # UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+# DELETE
+delete "tasks/:id", to: "tasks#destroy"
 
 end
